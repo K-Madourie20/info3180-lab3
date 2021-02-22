e469c8861ff389f1c3ef5c3dbd964333b0cc107b
K-Madourie20 <kodeymadourie8@gmail.com>
2021-02-21 21:23:47 -0500

    Making changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
-from flask_mail iport Mail
-from .config ipmort Config
+from flask_mail import Mail
+from .config import Config
+from flask_wtf.csrf import CSRFProtect
+csrf = CSRFProtect()
 
 app = Flask(__name__)
-app.config.from_object(Config)
+#app.config.from_object(Config)
+app.config['SECRET KEY'] ='Please provide the passphrase'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT']= '2525'
+app.config['MAIL_USERNAME']= 'Enter your mailtrap username'
+app.config['MAIL_PASSWORD']= 'Enter your mailtrap password'
 
 mail = Mail(app)
 from app import views
